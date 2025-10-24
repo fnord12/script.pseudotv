1d15fe18a5c35f9f60b2fd5ca60c7b315aa09129
fnord12 <fnord12@supermegamonkey.net>
2025-10-24 13:57:24 -0400

    totalTimePlayed for unwatched channel
    guythnick's fix for totalTimePlayed when channel hasn't been watched

--- ChannelList.py
+++ ChannelList.py
@@ -241,7 +241,10 @@ class ChannelList:
         
         if FileAccess.exists(CHANNELS_LOC + 'channel_' + str(channel) + '.m3u') and append == False and needsreset == False:
             #try:
-                self.channels[channel - 1].totalTimePlayed = int(float(ADDON_SETTINGS.getSetting('Channel_' + str(channel) + '_time', True)))
+                if ADDON_SETTINGS.getSetting('Channel_' + str(channel) + '_time', True):
+                    self.channels[channel - 1].totalTimePlayed = int(float(ADDON_SETTINGS.getSetting('Channel_' + str(channel) + '_time', True)))
+                else:
+                    self.channels[channel - 1].totalTimePlayed = 0
                 createlist = True
                 self.log('SETUPCHANNEL createlist set to True')
                 if self.background == False:
